318b14352b7bae11f7db19fa0351f73d99199200
rageragg <rageragg2004@gmail.com>
2023-11-01 10:15:14 -0400

    Actualizacion de Proceso de Fletes

--- db/indexes/FREIGHTS_CFG_UK.sql
+++ db/indexes/FREIGHTS_CFG_UK.sql
@@ -1,5 +1,5 @@
 --------------------------------------------------------
---  DDL for Index FREIGHTS_UK
+--  DDL for Index FREIGHTS_CFG_UK
 --------------------------------------------------------
 
 CREATE UNIQUE INDEX "IGTP"."FREIGHT_CFG_UK_01" 
@@ -7,7 +7,20 @@ CREATE UNIQUE INDEX "IGTP"."FREIGHT_CFG_UK_01"
         "CUSTOMER_ID", 
         "ROUTE_ID", 
         "TYPE_CARGO_ID", 
-        "TYPE_VEHICLE_ID", "K_REGIMEN"
+        "TYPE_VEHICLE_ID", 
+        "TYPE_FREIGHT_ID",
+        "K_REGIMEN"
+    ) 
+    PCTFREE 10 
+    INITRANS 2 
+    MAXTRANS 255 
+    COMPUTE STATISTICS 
+    TABLESPACE "TS_IGTP_DAT" ;
+
+CREATE UNIQUE INDEX "IGTP"."FREIGHT_CFG_UK_02" 
+    ON "IGTP"."FREIGHT_CFG" (
+        "CUSTOMER_ID", 
+        "CONTRACT_CO"
     ) 
     PCTFREE 10 
     INITRANS 2 
