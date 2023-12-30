9a1c388ae6ddc1ecdfdde4a4af5a882bfa4a6b2a
rageragg <rageragg2004@gmail.com>
2023-12-29 20:59:46 -0400

    Normalizacion de codigo y nombres de archivos

---
+++ db/ref_constraints/fk_municipalities.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  Ref Constraints for Table MUNICIPALITIES
+--------------------------------------------------------
+
+ALTER TABLE "IGTP"."MUNICIPALITIES" 
+  ADD CONSTRAINT "PROVINCE_MUNICIPALITY_FK" 
+  FOREIGN KEY ("PROVINCE_ID")
+  REFERENCES "IGTP"."PROVINCES" ("ID") ENABLE;
--- db/ref_constraints/fk_shops.sql
+++ db/ref_constraints/fk_shops.sql
@@ -2,7 +2,7 @@
 --  Ref Constraints for Table SHOPS
 --------------------------------------------------------
 
-  ALTER TABLE "IGTP"."SHOPS" 
-    ADD CONSTRAINT "LOCATION_SHOP_FK" 
-    FOREIGN KEY ("LOCATION_ID")
-	  REFERENCES "IGTP"."LOCATIONS" ("ID") ENABLE;
+ALTER TABLE "IGTP"."SHOPS" 
+  ADD CONSTRAINT "LOCATION_SHOP_FK" 
+  FOREIGN KEY ("LOCATION_ID")
+  REFERENCES "IGTP"."LOCATIONS" ("ID") ENABLE;
