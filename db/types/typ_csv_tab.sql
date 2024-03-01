9a0a96017fa94f1e44b4c233ae52ea802eaa2036
rageragg <rageragg2004@gmail.com>
2024-03-01 13:53:05 -0400

    Actualizacion Diaria

--- db/types/typ_csv_tab.sql
+++ db/types/typ_csv_tab.sql
@@ -1,7 +1,5 @@
 --------------------------------------------------------
 --  DDL for Type T_CSV_TAB
 --------------------------------------------------------
-
-  CREATE OR REPLACE NONEDITIONABLE TYPE "IGTP"."T_CSV_TAB" AS TABLE OF t_csv_line;
-
+CREATE OR REPLACE NONEDITIONABLE TYPE "IGTP"."T_CSV_TAB" AS TABLE OF t_csv_line;
 /
