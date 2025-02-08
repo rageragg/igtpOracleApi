913a245c21343de3f3d2fb60a90e128f2ff1d708
rageragg <rageragg2004@gmail.com>
2025-02-08 14:31:23 -0400

    Complementos de definicion de usuarios

--- db/constraint/USERS.sql
+++
@@ -1,7 +0,0 @@
---------------------------------------------------------
---  Constraints for Table USERS
---------------------------------------------------------
-
-  ALTER TABLE "IGTP"."USERS" ADD CONSTRAINT "USERS_PK" PRIMARY KEY ("ID")
-  USING INDEX "IGTP"."USERS_PK"  ENABLE;
-  ALTER TABLE "IGTP"."USERS" ADD CONSTRAINT "VALID_CHK" CHECK ( k_valid IN ('Y','N' ) ) ENABLE;
---
+++ db/sequences/sec/seq_users.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  DDL for Sequence USERS_SEQ
+--------------------------------------------------------
+
+CREATE SEQUENCE  "IGTP"."USERS_SEQ"  
+   MINVALUE 100 
+   MAXVALUE 9999999999999999999999999999 
+   INCREMENT BY 1 
+   START WITH 100 
+   CACHE 20 NOORDER NOCYCLE NOKEEP NOSCALE GLOBAL ;
