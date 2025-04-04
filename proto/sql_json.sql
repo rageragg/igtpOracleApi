e7b1e71ebf9537be282e8ec59fd3a910dbca62b3
rageragg <rageragg2004@gmail.com>
2025-04-04 07:39:39 -0400

    Prototipos de SQL-JSON

---
+++ proto/sql_json.sql
@@ -0,0 +1,23 @@
+        SELECT 
+           json_arrayagg(
+           json_object( 
+                'id' VALUE a.id,
+                'description' VALUE a.description,
+                'city_co' VALUE a.city_co,
+                'postal_co' VALUE a.postal_co,
+                'telephone_co' VALUE a.telephone_co,
+                'municipality_id' VALUE a.municipality_id,
+                'slug' VALUE a.slug,
+                'uuid' VALUE a.uuid,
+                'user_id' VALUE a.user_id,
+                'nu_gps_lat' VALUE a.nu_gps_lat,
+                'nu_gps_lon' VALUE a.nu_gps_lon,
+                'locations' VALUE json_arrayagg(b.id ORDER BY b.id)
+                FORMAT JSON
+            )) json_cities
+          FROM cities a 
+          INNER JOIN locations b ON a.id = b.city_id
+          WHERE a.municipality_id = 166
+          GROUP BY a.id, a.description, a.city_co, a.postal_co, 
+                   a.telephone_co, a.municipality_id, a.slug, a.uuid,
+                   a.user_id, a.nu_gps_lat, a.nu_gps_lon;
